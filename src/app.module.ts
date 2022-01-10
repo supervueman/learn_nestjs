3141051b61f5f9012a227121db02b84bd3923451
supervueman <supervueman@gmail.com>
2022-01-10 16:33:10 +0400

    Add mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { getMongoConfig } from './configs/mongo.config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,11 +6,17 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
     AuthModule,
     TopPageModule,
     ProductModule,
